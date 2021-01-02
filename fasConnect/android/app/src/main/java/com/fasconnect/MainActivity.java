14f7d58434706d6da344f16d885635cff1262541
truyenle16398 <55780534+truyenle16398@users.noreply.github.com>
2021-01-02 23:38:03 +0700

    Add Splash Screen and config full screen

--- fasConnect/android/app/src/main/java/com/fasconnect/MainActivity.java
+++ fasConnect/android/app/src/main/java/com/fasconnect/MainActivity.java
@@ -1,5 +1,9 @@
 package com.fasconnect;
 
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.View;
+
 import com.facebook.react.ReactActivity;
 
 public class MainActivity extends ReactActivity {
@@ -11,5 +15,26 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "fasConnect";
+  }
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    hideNavigationBar();
+  }
+  @Override
+  public void onWindowFocusChanged(boolean hasFocus) {
+    super.onWindowFocusChanged(hasFocus);
+    if (hasFocus) {
+      hideNavigationBar();
+    }
+  }
+
+  private void hideNavigationBar() {
+    DisplayMetrics metrics = new DisplayMetrics();
+    getWindowManager().getDefaultDisplay().getMetrics(metrics);
+      getWindow().getDecorView().setSystemUiVisibility(
+      View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+              | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+              | View.SYSTEM_UI_FLAG_FULLSCREEN);
   }
 }
